2c2c1a0b3bea4237f7e934b1a1a5976c2e18be2c
thayrov <thayrovg@gmail.com>
2023-01-20 21:07:23 -0600

    add feature to limit the input in StartGame to only numbers; and to put the keyboard away if needed

--- src/screens/start-game/index.jsx
+++ src/screens/start-game/index.jsx
@@ -1,29 +1,36 @@
-import {Button, Text, TextInput, View} from "react-native";
+import {Button, Keyboard, Text, TextInput, TouchableWithoutFeedback, View} from "react-native";
+import React, {useState} from "react";
 
 import { Card } from "../../components";
-import React from "react";
 import { colors } from "../../constants";
 import {styles} from './styles'
 
 export const StartGame = () => {
+    const [enteredValue, setEnteredValue] = useState('');
+    const OnHandlerChange = (text) => {
+        setEnteredValue(text.replace(/[^0-9]/g,''))
+    }
     return(
+    <TouchableWithoutFeedback onPress={()=>{Keyboard.dismiss()}}>
         <View style={styles.container}>
             <Text style={styles.title} >Start Game</Text>
             <Card style={styles.inputContainer}>
                 <Text style={styles.label} >Write a number</Text>
                 <TextInput 
+                value={enteredValue}
                 keyboardType="numeric" 
                 style={styles.input} 
                 placeholder='#'
                 placeholderTextColor={colors.TextColor}
+                onChangeText={OnHandlerChange}
                 />
             <View style={styles.buttonContainer}>
                 <Button title="Restart" onPress={()=>null} color={colors.WarningColor}/>
                 <Button title="Confirm" onPress={()=>null} color={colors.SecondaryColor}/>
             </View>
             </Card>
-
         </View>
+    </TouchableWithoutFeedback>
     );
 }
 
--- src/screens/start-game/styles.js
+++ src/screens/start-game/styles.js
@@ -7,6 +7,7 @@ export const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 		backgroundColor: colors.BGColor,
+		paddingBottom: 200,
 	},
 	title: {
 		fontSize: 24,
