19e69c7debc8be5c13815fc4f10dd585423f6b47
thayrov <thayrovg@gmail.com>
2023-01-25 12:15:13 -0600

    define basic functions to reset and confirm buttons

--- src/screens/start-game/index.jsx
+++ src/screens/start-game/index.jsx
@@ -1,4 +1,4 @@
-import {Button, Keyboard, Text, TextInput, TouchableWithoutFeedback, View} from "react-native";
+import {Alert, Button, Keyboard, Text, TextInput, TouchableWithoutFeedback, View} from "react-native";
 import React, {useState} from "react";
 
 import { Card } from "../../components";
@@ -7,9 +7,22 @@ import {styles} from './styles'
 
 export const StartGame = () => {
     const [enteredValue, setEnteredValue] = useState('');
+    const [confirmed, setConfirmed] = useState(false);
+    const [selectedNumber, setSelectedNumber] = useState(null);
+    
     const OnHandlerChange = (text) => {
         setEnteredValue(text.replace(/[^0-9]/g,''))
     }
+    const onHandlerReset = () => {
+        setEnteredValue('')
+        setConfirmed(false)
+    } 
+    const onHandlerConfirm = (enteredValue) => {
+        const chosenNumber = parseInt(enteredValue,10);
+        (isNaN(chosenNumber) || chosenNumber<=0 ||chosenNumber>99) ? 
+        Alert.alert('Wrong Number','Please use numbers between 1 and 99',[{text:'Got it', style: 'destructive', onPress:onHandlerReset}]) :
+        setConfirmed(true) && setSelectedNumber(chosenNumber) &&  setEnteredValue('')
+        }
     return(
     <TouchableWithoutFeedback onPress={()=>{Keyboard.dismiss()}}>
         <View style={styles.container}>
@@ -26,8 +39,8 @@ export const StartGame = () => {
                 maxLength={2}
                 />
             <View style={styles.buttonContainer}>
-                <Button title="Restart" onPress={()=>null} color={colors.WarningColor}/>
-                <Button title="Confirm" onPress={()=>null} color={colors.SecondaryColor}/>
+                <Button title="Restart" onPress={onHandlerReset} color={colors.WarningColor}/>
+                <Button title="Confirm" onPress={onHandlerConfirm} color={colors.SecondaryColor}/>
             </View>
             </Card>
         </View>
